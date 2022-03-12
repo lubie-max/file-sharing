0780323350267b7ccd220f680e57ae82f074f9f8
Lubna <shaikhls2421@gmail.com>
2022-03-12 11:24:39 +0530

    pushing commits

--- filesharingapp/settings.py
+++ filesharingapp/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p!&4(q_sibk82xtyzyshj0*bz&786!lnqc!s_!ab3j4*wdujnx'
+SECRET_KEY =  os.environ['SECRET_KEY'] 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['todo-lubie.herokuapp.com','localhost']
 
 
 # Application definition
